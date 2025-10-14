e17f4822f129906e4691cc191b9bb227ae1593bb
Biel Martínez <biel.martinez@estudiants.urv.cat>
2025-10-14 11:55:31 +0200

    prog2: 1C

--- source/candy1_secu.s
+++ source/candy1_secu.s
@@ -39,11 +39,84 @@
 @;		R0 = 1 si hay una secuencia, 0 en otro caso
 	.global hay_secuencia
 hay_secuencia:
-		push {lr}
-		
-		
-		pop {pc}
-
+		push {r1-r11,lr}
+		mov r1, #0                     	@; r1 = i
+		mov r2, #0                     	@; r2 = j
+		mov r3, #ROWS                  	@; r3 = Filas
+		mov r4, #COLUMNS               	@; r4 = Columnas
+		sub r8, r3, #2				   	@; r8 = filas-2-->7
+		sub r9, r4, #2                 	@; r9 = columnas-2-->7
+	.Lfor1:
+		cmp r1, r3						@; i < filas
+		bhs .Lfifor1
+		mov r2, #0
+	.Lfor2:
+		cmp r2, r4						@; j < columnas
+		bhs .Lfifor2
+		mla r6, r1, r4, r2				@; R6 = i * NC + j
+		add r7, r0, r6
+		ldrb r5,[r7]					@; R5 = matriz[i][j]
+	.Lif1:
+		tst r5, #0x07					@; Comprobar que el valor no sea un espacio vacio
+		beq .Lfiif1
+		mvn r5, r5
+		tst r5, #0x07  					@; Comprobar que no sea un bloque solido o un hueco
+		beq .Lfiif1
+	.Lif2:
+		cmp r1, r8						@; i < filas-2
+		bhs .Lelse22
+		cmp r2, r9						@; j < columnas-2
+		bhs .Lelse2
+	.Lif3:
+		mov r10, r3						@; Guardar filas en R10
+		mov r11, r0						@; Guardar dirección base de la matriz de juego en R11
+		mov r3, #1						@; Añadir dirección(sur) en R3
+		bl cuenta_repeticiones			@; Llamar funcion cuenta repeticiones
+		cmp r0, #3						@; nº de repiticiones >= 3
+		blo .Lelse3
+		b .Lreturn1
+	.Lelse3:
+		mov r0, r11    					@; Devolver dirección base de la matrix de juego en R0
+		mov r3, #0						@; Añadir dirección(este) en R3
+		bl cuenta_repeticiones			@; Llamar funcion cuenta repeticiones
+		cmp r0, #3						@; nº de repiticiones >= 3
+		blo .Lfiif2
+		b .Lreturn1
+	.Lelse2:
+	.Lif4:
+		mov r10, r3						@; Guardar filas en R10
+		mov r11, r0						@; Guardar dirección base de la matriz de juego en R11
+		mov r3, #1						@; Añadir dirección(este) en R3
+		bl cuenta_repeticiones			@; Llamar funcion cuenta repeticiones
+		cmp r0, #3						@; nº de reèticiones >= 3
+		blo .Lfiif2
+		b .Lreturn1
+	.Lelse22:
+		cmp r2, r9						@; j >= colimnas-2
+		bhs .Lfiif1
+		mov r10, r3						@; Guardar filas en R10
+		mov r11, r0						@; Guardar dirección base de la matriz de juego en R11
+		mov r3, #0						@; Añadir dirección(sur) en R3
+		bl cuenta_repeticiones			@; Llamar funcion cuenta repeticiones
+		cmp r0, #3						@; nº de repeticiones >= 3
+		blo .Lfiif2
+		b .Lreturn1
+	.Lfiif2:	
+		mov r0, r11						@; Devolver dirección base de la matrix de juego en R0 
+		mov r3, r10						@; Devolver filas en R3
+	.Lfiif1:
+		add r2, #1						@; j++
+		b .Lfor2						@; Saltar al segundo bucle
+	.Lfifor2:
+		add r1, #1						@; i++
+		b .Lfor1						@; Saltar al primer bucle
+	.Lfifor1:
+		mov r0, #0						@; Hay secuencias = 0 (false)
+		b .Lreturn0
+	.Lreturn1:
+		mov r0, #1						@; Hay secuencias = 1 (true)
+	.Lreturn0:
+		pop {r1-r11,pc}
 
 @;TAREA 1D;
 @; elimina_secuencias(*matriz, *marcas): rutina para eliminar todas las
